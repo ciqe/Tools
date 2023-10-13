86fe3a430b7ff08bb6cceeeb26cb208eb1fc461a
Name less User <106626236+ciqe@users.noreply.github.com>
2023-10-13 15:01:34 -0400

    Create install-in-kali.sh

---
+++ Golang-Tools/install-in-kali.sh
@@ -0,0 +1,8 @@
+echo -e "\n\n\n\n\nInstalling arjun..."
+sudo apt install arjun
+
+echo -e "\n\n\n\n\nInstalling ZAP..."
+sudo apt install zaproxy
+
+echo -e "\n\n\n\n\nInstalling Seclists..."
+sudo apt install seclists
